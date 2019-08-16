64e1096147c3001e52c747e20838bb1937d40c4c
ade-putra-alfauzi <adeputraalfauzi1@gmail.com>
2019-08-16 11:16:04 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -7,5 +7,9 @@
 <body>
 	<h1>Hello World</h1>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, neque non ipsa quis labore commodi beatae ipsam ducimus placeat iure distinctio quibusdam, ullam velit, eius itaque recusandae earum vel provident.</p>
+	<ul>
+		<li>satu</li>
+		<li>dua</li>
+	</ul>
 </body>
 </html>
